b147d7fc5d8469303bf2fe37259e298cced55957
Daniel Casalli <danis.settk@gmail.com>
2025-03-28 11:16:13 -0300

    feat(operadores-aritmeticos):4 e 8

---
+++ aocubo.js
@@ -0,0 +1,4 @@
+let numero = 2;
+let quadrado = (numero * numero);
+let cubo = (numero * [numero * numero]);
+console.log(numero + " ao quadrado é " + quadrado + " e ao cubo é " + cubo);
--- area.js
+++ area.js
@@ -1,4 +1,4 @@
 let altura = 6;
 let base = 5;
 let area = (base * altura);
-console.log("O espaço da área é " + area)
+console.log("O espaço da área é " + area + " metros")
