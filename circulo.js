89f04fb32c0f45fdf1513a6bde45724dc0c5412d
daniel <danis.settk@gmail.com>
2025-03-28 11:34:00 -0300

    circulo.js

--- circulo.js
+++ circulo.js
@@ -2,4 +2,4 @@ let raio = 7;
 let pi = 3.14;
 let calculo = (raio * raio);
 let area = (calculo * pi );
-console.log("Me sinto orgulhoso de achar o raio do circulo, que é " + area);
+console.log("Me sinto orgulhoso de achar a área do circulo, que é " + area);
