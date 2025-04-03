e273065e2851f9efef67068f5799e388992e5e51
daniel <danis.settk@gmail.com>
2025-04-03 19:05:30 -0300

    feat(if-else): adiciona classificação etária por idade

---
+++ ifelse/idade.js
@@ -0,0 +1,10 @@
+let idade =70;
+if(idade >=0 && idade <= 12){
+    console.log("É criança mané");
+} else if (idade >= 13 && idade <= 17){
+    console.log("É adolescente mané");
+} else if (idade >= 18 && idade <= 59 ){
+    console.log("E adolto mané");
+} else {
+    console.log("É veio mané");
+}
