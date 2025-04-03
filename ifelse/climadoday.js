57d1e7fd0b4766af41f7595f3446d71d7f395dc3
daniel <danis.settk@gmail.com>
2025-04-03 19:09:41 -0300

    feat(if-else): adiciona classificação de clima por temperatura

---
+++ ifelse/climadoday.js
@@ -0,0 +1,8 @@
+let temperatura = 29;
+if(temperatura <= 15){
+    console.log("Mané ta frio ");
+} else if (temperatura >= 16 && temperatura <= 25){
+    console.log("Mané ta normalzim o clima ta agradavel em");
+} else {
+    console.log("Mané ta calor em")
+}
