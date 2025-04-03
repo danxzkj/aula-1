45e4d5b7fb3b0f03f67a44ad703e4a07d06df4bb
daniel <danis.settk@gmail.com>
2025-04-03 17:46:05 -0300

    feat(operadores-logicos): adiciona operação AND com booleanos

---
+++ sism/sism/AND.js
@@ -0,0 +1,9 @@
+let rapido = 'false';
+let ganhei = 'true';
+if(rapido = 'true' && ganhei == 'true'){
+    console.log("Eu ganhei e ainda sou bem rapido");
+} else if(rapido = 'false' && ganhei == 'false') {
+    console.log("Alem de lerdo ainda perdi");
+} else {
+    console.log("Nem participei da corrida");
+}
