19fbf627c584b4345583c5ecb7283f8947533841
daniel <danis.settk@gmail.com>
2025-04-03 19:03:03 -0300

    feat(if-else): adiciona verificação de positivo, negativo ou zero

---
+++ ifelse/vddoufalse.js
@@ -0,0 +1,8 @@
+let numero = 0;
+if(numero >= 1){
+    console.log("O numero é positivo mané");
+} else if (numero <= -1){
+    console.log("É negativo meu");
+} else {
+    console.log("É zeroooo");
+}
