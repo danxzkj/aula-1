daeb28a1508e85dcdc7136939b4f85d5b21ee7d2
daniel <danis.settk@gmail.com>
2025-04-03 19:08:26 -0300

    feat(if-else): adiciona cálculo de desconto baseado em salário

---
+++ ifelse/desconto.js
@@ -0,0 +1,8 @@
+let salario = 2000;
+if (salario >= 2000){
+    console.log("Mano seu salario ficou " + 2000 * 0.90);
+} else if (salario < 2000) {
+    console.log("Mano o salario ficou " + 2000 * 0.95);
+} else {
+    console.log("Não é dia de pagamento");
+}
