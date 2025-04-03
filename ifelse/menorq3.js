0b15faa16debce715aa6a85def80dd7d7bbbb386
daniel <danis.settk@gmail.com>
2025-04-03 19:09:11 -0300

    feat(if-else): adiciona identificação do menor entre três números

---
+++ ifelse/menorq3.js
@@ -0,0 +1,10 @@
+let numero = 5;
+let numero2 = 4;
+let numero3 = 3;
+if (numero < numero3 && numero < numero2){
+    console.log("Mané o numero " + numero + "é o menor tlg ");
+} else if (numero2 < numero && numero2 < numero3){
+    console.log("Mané o numero " + numero2 + " é o menor")
+}else{
+    console.log("Mané o numero " + numero3 + " é o menor tlg")
+}
