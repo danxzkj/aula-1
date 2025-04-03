20be4d7062aa1fcb7af38fcbfeb219250eaa4ce8
daniel <danis.settk@gmail.com>
2025-04-03 13:58:58 -0300

    feat(operadores-logicos): adiciona verificação de número maior que 100

---
+++ sism/+100.js
@@ -0,0 +1,8 @@
+let numero = 123;
+let numero2 = 112;
+let numero3 = 199;
+if(numero > 100 && numero2 > 100 && numero > 100){
+    console.log("Um ou todos deles é maior que 100 ou igual");
+} else {
+    console.log("Nem um é maio q 100");
+}
