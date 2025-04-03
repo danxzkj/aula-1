9d05f67842db0846d199dfe1ce4a15d695377000
daniel <danis.settk@gmail.com>
2025-04-03 20:14:49 -0300

    feat(switch-case): adiciona exibição do dia da semana

---
+++ ifelse/Switch Case/daydasemana.js
@@ -0,0 +1,26 @@
+let dia = 6;
+switch(dia){
+    case(dia = 1):
+    console.log("É segunda mané");
+    break;
+    case(dia = 2):
+    console.log("E terça mané");
+    break;
+    case(dia = 3):
+    console.log("É quarta mané");
+    break;
+    case(dia = 4):
+    console.log("É quinta mané");
+    break;
+    case(dia = 5):
+    console.log("É sexta mané");
+    break;
+    case(dia = 6):
+    console.log("É sabado mané");
+    break
+    case(dia = 7):
+    console.log("É domingo mané");
+    break;
+    default:
+        ("Numero invalido mané");
+} 
