a97e71f257bc87ab9ba70d0780ed008cb4e77a64
daniel <danis.settk@gmail.com>
2025-04-03 20:17:18 -0300

    feat(switch-case): adiciona exibição da estação do ano

---
+++ Switch Case/estaçãodoano.js
@@ -0,0 +1,17 @@
+let estação = 1
+switch(estação){
+    case(estação = 1):
+    console.log("É verão mané");
+    break;
+    case(estação = 2):
+    console.log("É outono mané");
+    break;
+    case(estação = 3):
+    console.log("É primavera mané");
+    break;
+    case(estação = 4):
+    console.log("É inverno mané");
+    break;
+    default:
+        console.log("Esse numero não tem mané");
+}
