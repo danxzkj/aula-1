c1448f16f0965a4a0866c4a13dd255dc1475c566
daniel <danis.settk@gmail.com>
2025-04-03 19:04:08 -0300

    feat(if-else): adiciona comparação do maior entre dois números

---
+++ ifelse/ifelse/maiorvence.js
@@ -0,0 +1,9 @@
+let numero1 = 0;
+let numero2 = 8;
+if(numero1 > numero2){
+    console.log("O primeiro numero é maior tlg");
+} else if(numero1 < numero2) {
+    console.log("O segundo numero é maior né mané");
+} else {
+    console.log("Ixi são iguais");
+}
