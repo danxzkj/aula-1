38337570d96c038f349098b48d62e7306a82eb37
daniel <danis.settk@gmail.com>
2025-04-03 20:15:38 -0300

    feat(switch-case): adiciona exibição do mês do ano

---
+++ Switch Case/mesesdoano.js
@@ -0,0 +1,41 @@
+let mes = 2;
+switch(mes){
+    case(mes = 1):
+    console.log("É janeiro mané");
+    break;
+    case(mes = 2):
+    console.log("É fevereiro mané");
+    break;
+    case(mes = 3):
+    console.log("É março mané");
+    break;
+    case(mes = 4):
+    console.log("É abril mané");
+    break;
+    case(mes = 5):
+    console.log("É maio mané");
+    break;
+    case(mes = 6):
+    console.log("É junho mané");
+    break;
+    case(mes = 7):
+    console.log("É julho mané");
+    break;
+    case(mes = 8):
+    console.log("É agosto mané");
+    break;
+    case(mes = 9):
+    console.log("É setembro mané");
+    break;
+    case(mes = 10):
+    console.log("É outubro mané");
+    break;
+    case(mes = 11):
+    console.log("É novembro mané");
+    break;
+    case(mes = 12):
+    console.log("É dezembro mané");
+    break;
+    default:
+        console.log("Existe esse ano não");
+} 
